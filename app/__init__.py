ec5aed50a05cbd9c40174000b45cddf975dc2bf3
fjebaker <fergusbkr@gmail.com>
2022-03-19 17:17:42 +0000

    black formatting

--- app/__init__.py
+++ app/__init__.py
@@ -10,13 +10,13 @@ from flask import g
 def create_app():
     app = Flask(__name__)
 
-    # use the development configuration if FLASK_ENV == 'production'
+    # use the development configuration if FLASK_ENV == 'production'
     if app.config["ENV"] == "production":
         app.config.from_object("app.flask_config.Production")
     else:
         app.config.from_object("app.flask_config.Development")
 
-    # register CORS
+    # register CORS
     CORS(app)
 
     # register database
--- app/flask_config.py
+++ app/flask_config.py
@@ -2,20 +2,23 @@ import os
 
 SECRET_KEY = os.urandom(1024)
 
+
 class Config(object):
-    MARIADB_USER = 'aimlac'
-    MARIADB_PASSWORD = 'aimlac'
-    MARIADB_DATABASE = 'llanwrytd'
+    MARIADB_USER = "aimlac"
+    MARIADB_PASSWORD = "aimlac"
+    MARIADB_DATABASE = "llanwrytd"
     SECRET_KEY = SECRET_KEY
     MARIADB_PORT = 3306
+
 
 class Development(Config):
     DEBUG = True
     TESTING = True
-    FLASK_ENV = 'development'
-    MARIADB_HOST = '127.0.0.1'
+    FLASK_ENV = "development"
+    MARIADB_HOST = "127.0.0.1"
+
 
 class Production(Config):
     DEBUG = False
     TESTING = False
-    MARIADB_HOST = 'aimlac-database'
+    MARIADB_HOST = "aimlac-database"
