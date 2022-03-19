3746022ae5b3f4293da11e8f32cc840756681307
fjebaker <fergusbkr@gmail.com>
2022-03-19 17:17:11 +0000

    single database connection
    in the future, this should be ported to use a connection pool if we
    anticipate heavy traffic

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
+from app.database import init_app_database
 
 import app.power_routes as power
+
+from flask import g
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile("flask_config.py")
 
+    # use the development configuration if FLASK_ENV == 'production'
+    if app.config["ENV"] == "production":
+        app.config.from_object("app.flask_config.Production")
+    else:
+        app.config.from_object("app.flask_config.Development")
+
+    # register CORS
     CORS(app)
+
+    # register database
+    init_app_database(app)
 
     app.register_blueprint(power.bp)
 
--- app/flask_config.py
+++ app/flask_config.py
@@ -1,3 +1,21 @@
-from os import urandom
+import os
 
-SECRET_KEY = urandom(1024)
+SECRET_KEY = os.urandom(1024)
+
+class Config(object):
+    MARIADB_USER = 'aimlac'
+    MARIADB_PASSWORD = 'aimlac'
+    MARIADB_DATABASE = 'llanwrytd'
+    SECRET_KEY = SECRET_KEY
+    MARIADB_PORT = 3306
+
+class Development(Config):
+    DEBUG = True
+    TESTING = True
+    FLASK_ENV = 'development'
+    MARIADB_HOST = '127.0.0.1'
+
+class Production(Config):
+    DEBUG = False
+    TESTING = False
+    MARIADB_HOST = 'aimlac-database'
