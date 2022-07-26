987fee5000254c1f0c84421d9adaffb6251d6925
Ding Sheng Ong <dso@aber.ac.uk>
2022-07-26 04:14:55 +0100

    add price prediction to bidder

--- src/bidding/slimjab_bidder.py
+++ src/bidding/slimjab_bidder.py
@@ -14,18 +14,25 @@ from src.bidding.util import get_output_template, register_bidder
         "end_date": date.today() + timedelta(days=2),
     },
     data={
-        "power": 'SELECT time, (WindPower + SolarPower - HQPowerDemand) * 1000 AS NetPower FROM powerPrediction WHERE time > "{start_date}" AND time < "{end_date}"'
+        "power": 'SELECT time, (WindPower + SolarPower - HQPowerDemand) * 1000 AS NetPower FROM powerPrediction WHERE time > "{start_date}" AND time < "{end_date}"',
+        "price": 'SELECT * FROM pricePrediction WHERE time > "{start_date}" AND time < "{end_date}"',
     },
     default=True,
 )
 def slimjab_bidder(**kwargs):
     power = kwargs["power"].set_index("time")
+    price = kwargs["price"].set_index("time")
     df = get_output_template()
     for i in range(len(df)):
         time = np.datetime64(
             f'{df.loc[i, "applying_date"]} {str(df.loc[i, "hour_ID"] - 1).zfill(2)}'
         )
-        if not f"{time}:00:00" in power.index or not f"{time}:30:00" in power.index:
+        if (
+            not f"{time}:00:00" in power.index
+            or not f"{time}:30:00" in power.index
+            or not f"{time}:00:00" in price.index
+            or not f"{time}:30:00" in price.index
+        ):
             df = df.drop(i)
             continue
 
@@ -33,9 +40,11 @@ def slimjab_bidder(**kwargs):
             power.loc[f"{time}:00:00", "NetPower"]
             + power.loc[f"{time}:30:00", "NetPower"]
         ) / 2
+        estimatedPrice = (
+            price.loc[f"{time}:00:00", "price"]
+            + price.loc[f"{time}:30:00", "price"]
+        ) / 2
         df.loc[i, "volume"] = abs(volume)
-        df.loc[i, "price"] = (
-            random.random() * 100 + 100
-        )  # random values between 100 - 200
+        df.loc[i, "price"] = estimatedPrice
         df.loc[i, "type"] = "BUY" if volume < 0 else "SELL"
     return df
