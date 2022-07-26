d5e88df03a5d55c8e604c1572fafd4bbb600031f
Ding Sheng Ong <dso@aber.ac.uk>
2022-07-26 03:57:23 +0100

    add slimjab price prediction

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,5 +1,6 @@
 import server.routes.bidding as bidding
 import server.routes.predict_power as predict_power
+import server.routes.predict_price as predict_price
 
 # create binding to export
-routes = [bidding.bp, predict_power.bp]
+routes = [bidding.bp, predict_power.bp, predict_price.bp]
