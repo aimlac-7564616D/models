488f213fdd76ea6d37415c2b01cbb1e958edc214
Ding Sheng Ong <dso@aber.ac.uk>
2022-08-27 17:33:40 +0100

    changed HTTP method

--- server/routes/co2.py
+++ server/routes/co2.py
@@ -3,7 +3,7 @@ from src.co2.co2_saved import co2_saved
 
 bp = Blueprint("co2", __name__, url_prefix="/co2")
 
-@bp.route("/set", methods=["POST"])
+@bp.route("/set", methods=["GET"])
 def setCO2Saved():
     try:
         co2 = co2_saved()
