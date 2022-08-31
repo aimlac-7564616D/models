fix bidder price computation

src/bidding/slimjab_bidder.py
@@ -23,15 +23,16 @@ def slimjab_bidder(**kwargs):
     power = kwargs["power"].set_index("time")
     price = kwargs["price"].set_index("time")
     df = get_output_template()
+    print(power, price, df)
     for i in range(len(df)):
         time = np.datetime64(
             f'{df.loc[i, "applying_date"]} {str(df.loc[i, "hour_ID"] - 1).zfill(2)}'
         )
+        print(f"{time}:00:00", f"{time}:00:00" in power.index)
         if (
             not f"{time}:00:00" in power.index
             or not f"{time}:30:00" in power.index
             or not f"{time}:00:00" in price.index
-            or not f"{time}:30:00" in price.index
         ):
             df = df.drop(i)
             continue
@@ -40,9 +41,7 @@ def slimjab_bidder(**kwargs):
             power.loc[f"{time}:00:00", "NetPower"]
             + power.loc[f"{time}:30:00", "NetPower"]
         ) / 2
-        estimatedPrice = (
-            price.loc[f"{time}:00:00", "price"] + price.loc[f"{time}:30:00", "price"]
-        ) / 2
+        estimatedPrice = price.loc[f"{time}:00:00", "price"]
         df.loc[i, "volume"] = abs(volume)
         df.loc[i, "price"] = estimatedPrice
         df.loc[i, "type"] = "BUY" if volume < 0 else "SELL"